b9795f14879b6f53689d34bca3fea74c09339161
yurisanderaj1212 <167145134+yurisanderaj1212@users.noreply.github.com>
2024-12-12 03:42:34 +0100

    change database.config to folder config and create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfig), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
