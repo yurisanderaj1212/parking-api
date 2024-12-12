3434a02564f55b7a7ded5d49dc57921cfa8779be
yurisanderaj1212 <167145134+yurisanderaj1212@users.noreply.github.com>
2024-12-12 03:36:43 +0100

    database config

---
+++ src/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'parking_api',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
